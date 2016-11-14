6a5b51a6ccaa78361670defe62c18d83ed157551
johnwcallahan <johnwcallahan@gmail.com>
2016-11-13 19:42:38 -0800

    clean up alignment

--- script.js
+++ script.js
@@ -213,21 +213,21 @@ $(document).ready(function() {
 
     //Maps HTML IDs to operands and operators
     var opMap = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9",
-        "point": ".",
-        "plus": " + ",
-        "minus": " - ",
+        "zero":     "0",
+        "one":      "1",
+        "two":      "2",
+        "three":    "3",
+        "four":     "4",
+        "five":     "5",
+        "six":      "6",
+        "seven":    "7",
+        "eight":    "8",
+        "nine":     "9",
+        "point":    ".",
+        "plus":     " + ",
+        "minus":    " - ",
         "multiply": " * ",
-        "divide": " / "
+        "divide":   " / "
     };
 
     $(".digit").click(function(event) {
